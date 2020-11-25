eabff84fbfc2e188d81fba1d34eb7080af29cd6f
unknown <elimm101996@gmail.com>
2020-11-25 17:18:18 +0100

    cambiando fuente

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,7 +28,7 @@ img{
     z-index: -1;
   }
   .circle:before{
-      width:100%;
+      width:110%;
       height:700px;
       ${'' /* border-radius:0 0 50% 50%; */}
       border-bottom-left-radius: 100%;
@@ -41,10 +41,10 @@ img{
   }
   h1{
       ${'' /* position: absolute; */}
-      margin-top:15%;
+      margin-top:5%;
       ${'' /* bottom:200px; */}
       text-align: center;
-      font-size:50px;
+      font-size:20px;
       color:white;
   }
   .outline{
